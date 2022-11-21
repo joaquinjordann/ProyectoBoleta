a9b64418cef6ac9b7a75e4c3ed32daeb0a17a6b0
joaquinjordann <83792927+joaquinjordann@users.noreply.github.com>
2022-11-21 06:50:54 -0600

    Delete ProyectoBoleta directory

--- ProyectoBoleta/back.php
+++
@@ -1,3 +0,0 @@
-<?php
-include("location:login.php");
-?>
--- ProyectoBoleta/menuScript.js
+++
@@ -1,9 +0,0 @@
-let sidebar = document.querySelector(".sidebar");
-let sidebarBtn = document.querySelector(".flecha_izquierda");
-sidebarBtn.onclick = function() {
-  sidebar.classList.toggle("active");
-  if(sidebar.classList.contains("active")){
-  sidebarBtn.classList.replace(".flecha_izquierda" ,"flecha_derecha");
-}else
-  sidebarBtn.classList.replace("flecha_derecha", ".flecha_izquierda");
-}
